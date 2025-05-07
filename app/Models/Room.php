af481b3861cf31ed08f2cbc7b04b5350920b1b6a
rhmt25 <rhmtsekolah@gmail.com>
2025-05-07 18:29:18 +0700

    api bug #1

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    //
+    protected $table = 'ruangan';         // Nama tabel di DB
+    protected $primaryKey = 'id_ruangan'; // Primary key custom
+
+    public $timestamps = false; // Jika tidak pakai created_at dan updated_at
+
+    protected $fillable = [
+        'nama_ruangan',
+        'kapasitas',
+        'fasilitas',
+        'status',
+        'harga',
+    ];
 }
