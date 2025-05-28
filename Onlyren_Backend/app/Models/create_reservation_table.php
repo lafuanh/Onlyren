106a2844af4c58a8bf94a3a1048b24a76974aedc
rhmt25 <rhmtsekolah@gmail.com>
2025-05-28 21:22:40 +0700

    Menambahkan migrasi database

---
+++ Onlyren_Backend/app/Models/create_reservation_table.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class create_reservation_table extends Model
+{
+    //
+}
---
+++ Onlyren_Backend/database/migrations/2025_05_28_140456_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('room_id')->constrained('rooms')->onDelete('cascade');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('duration');
+            $table->enum('status', ['Pending', 'Payment', 'Completed'])->default('Pending');
+            $table->integer('guests');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
